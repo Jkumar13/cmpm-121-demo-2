3b45b9f419a7d422bd312f8cb3e37650faccfa4c
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-11-22 19:09:13 -0800

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,16 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// create element for the title
+const appTitle = document.createElement("h1");
+appTitle.textContent = "app title";
+
+//create canvas element with 256x256 pixels size
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+// append both elements to the document body
+document.body.appendChild(appTitle);
+document.body.appendChild(canvas);
