43b1652a903071a69881fb64a08351c1d8ff7250
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-11-22 20:43:30 -0800

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,56 @@ canvas.height = 256;
 // append both elements to the document body
 document.body.appendChild(appTitle);
 document.body.appendChild(canvas);
+
+
+// create clear button
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear Canvas";
+
+document.body.appendChild(clearButton);
+
+// 2d drawing context of canvas
+const ctx = canvas.getContext("2d");
+
+// establishing variables
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+// mouse events
+function startDrawing(event: MouseEvent) {
+    isDrawing = true;
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+}
+
+function draw(event: MouseEvent) {
+    if (!isDrawing) return;
+
+    const currentX = event.offsetX;
+    const currentY = event.offsetY;
+
+    // draw lines
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(currentX, currentY);
+    ctx.stroke();
+
+    lastX = currentX;
+    lastY = currentY;
+}
+
+function stopDrawing() {
+    isDrawing = false;
+}
+
+// event listeners
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseleave", stopDrawing);
+
+// clear buutton event listener
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
